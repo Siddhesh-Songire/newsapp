ff47458da568dea4be58b79ac7b29513db13ae85
Siddhesh <siddhehsongire@gmail.com>
2022-07-23 14:51:00 +0530

    Added Bootstrap cdn and created components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 
 // RCC
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+// import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
         <h1> Welcome to newsapp</h1>
+        <News/>
       </div>
     )
   }
