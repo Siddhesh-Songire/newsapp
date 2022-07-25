3d9f2070c1edf8be13c8a5c7dc5ab9fedfbce744
Siddhesh <siddhehsongire@gmail.com>
2022-07-25 18:26:27 +0530

    Added cards for displaying news in newsitem.js.

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
   render() {
     return (
-      <div>News
-      <NewsItem/>
+      <div className="container my-4">
+        <h2>NewsMonkey - Top headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem
+              title="My title"
+              description="this is description using props"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="My title"
+              description="this is description using props"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="My title"
+              description="this is description using props"
+            />
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem
+              title="My title"
+              description="this is description using props"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="My title"
+              description="this is description using props"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="My title"
+              description="this is description using props"
+            />
+          </div>
+        </div>
       </div>
-
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let {title,description} = this.props;  // destructing and props
     return (
-      <div>NewsItem</div>
-    )
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="#" className="btn btn-primary">
+              Know More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
