da0e3e12d68a4e80c2c9b3aa88bac07aa4c4dd8a
Siddhesh <siddhehsongire@gmail.com>
2022-08-03 20:01:00 +0530

    Looing through an array in JSX to display newsItems.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title,description} = this.props;  // destructing and props
+    let {title,description,imageURL, newsUrl} = this.props;  // destructing and props
     return (
-      <div>
+      <div className = "my-2">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..." />
+          <img src = {imageURL} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}...</h5>
             <p className="card-text">
-              {description}
+              {description}...
             </p>
-            <a href="#" className="btn btn-primary">
-              Know More
+            <a href = {newsUrl} target = "_blank" className="btn btn-primary btn-sm">
+              Read More
             </a>
           </div>
         </div>
